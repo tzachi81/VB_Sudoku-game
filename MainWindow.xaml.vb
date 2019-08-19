3bd1a07bc2f4dde1f10a153be0b9d436a34b1629
Tzachi Halevy <tzachi81@users.noreply.github.com>
2019-08-19 22:44:25 +0300

    Update MainWindow.xaml.vb

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,10 +1,9 @@
 ﻿Imports System.Data
 Class MainWindow
-    Public Shared mysql = New DB_IntegrationMethods 'ייבוא אינסטנס משותף של טיפול במסד הנתונים
-    Public Shared playerName As String  ' משתנה  ---משותף לכל החלונות בפרויקט --- הקולט את שם השחקן המוקלד בתיבת הטקסט 
-    Public Shared getlevel As String  'משתנה  ---משותף לכל החלונות בפרויקט --- הקולט את רמת הקושי שבחר השחקן מכפתורי הרדיו
-    '*****************************************************************************************************************
-    'פונקצייה לבדיקת תקינות ההקלדה של שם השחקן בתיבת הטקסט
+    Public Shared mysql = New DB_IntegrationMethods 
+    Public Shared playerName As String  
+    Public Shared getlevel As String 
+    
     Function nameCheck() As Boolean
         playerName = name.Text
         Dim check As Boolean = False
@@ -15,10 +14,8 @@ Class MainWindow
         End If
         Return check
     End Function
-    '*********************טיפול בכפתורים*************************
 
-    '<<<<כפתור הצגת השחקנים ופרטיהם על גבי חלון המשחק
-    Dim ds As DataSet 'הגדרת טיפוס להכלת טבלאות של בסיסי נתונים
+    Dim ds As DataSet 
     Private Sub btnFame_Click(sender As Object, e As RoutedEventArgs) Handles btnFame.Click
         If MainWindow.getlevel = Nothing Then
             lblMsg.Content = "first choose a level"
@@ -29,7 +26,6 @@ Class MainWindow
         End If
     End Sub
 
-    '<<<<<<כפתור יציאה מהתכנית
     Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs) Handles btnExit.Click
         Dim rushure2 As MsgBoxResult = MessageBox.Show("Are you sure you want to EXIT?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question)
         If rushure2 = vbYes Then
@@ -37,30 +33,28 @@ Class MainWindow
         End If
     End Sub
 
-    '<<<<<כפתור הוראות המשחק
     Private Sub btnHelp_Click(sender As Object, e As RoutedEventArgs) Handles btnHelp.Click
         Dim hlp As New help
         hlp.Show()
     End Sub
 
-    '***************כפתורי הרדיו*****************
-    '<<<<רמה 1- קל
+    'Level 1 button
     Private Sub RadioButton_checked_1(sender As Object, e As RoutedEventArgs)
         getLevel = "easy"
         btnPlay.Visibility = Windows.Visibility.Visible
     End Sub
-    '<<<<רמה 2- בינוני
+    'Level 2 button
     Private Sub RadioButton_checked_2(sender As Object, e As RoutedEventArgs)
         getLevel = "medium"
         btnPlay.Visibility = Windows.Visibility.Visible
     End Sub
-    '<<<<רמה 3- קשה
+    'Level 3 button
     Private Sub RadioButton_checked_3(sender As Object, e As RoutedEventArgs)
         getLevel = "hard"
         btnPlay.Visibility = Windows.Visibility.Visible
     End Sub
 
-    '<<<כפתור התחלת משחק - מוביל לחלון המשחק
+    'Begin game button
     Private Sub btnPlay_Click(sender As Object, e As RoutedEventArgs) Handles btnPlay.Click
         If nameCheck() = True Then
             Dim startgame As New Game
